6b39ce86a1c800384e08614bd65eb7a6516f9d4d
Muhammed <Olumide2956@gmail.com>
2023-05-20 04:20:07 -0700

    Finally

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+/**
+ * main - Prints puts
+ *
+ * Description: prints "function followed by a new line"
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
