5c3a9bee2fbbaf8693144eed1799fd75c6beef35
Muhammed <Olumide2956@gmail.com>
2023-05-21 02:08:10 -0700

    Bullied by whitespaces/betty

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints printf function 
+ *
+ * Description - prints "the main function"
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
