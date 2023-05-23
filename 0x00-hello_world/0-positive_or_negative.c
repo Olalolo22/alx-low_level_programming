6236be37c2d31d7b316de85968d792806ac9cce3
Muhammed <Olumide2956@gmail.com>
2023-05-22 21:19:44 -0700

    GOD

---
+++ 0x00-hello_world/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Prints result using if and else statement
+ * Description - prints "negative positive and zero using if and else"
+ * Return: 0 success
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		if (n > 0)
+		{
+			printf("%d is positive\n", n);
+		}
+		else
+		{
+			printf("%d is zero\n", n);
+		}
+	}
+	return (0);
+}
