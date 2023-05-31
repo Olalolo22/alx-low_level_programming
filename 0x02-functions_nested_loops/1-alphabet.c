691dddf393c49f9e2b2d410b465725dcc1a578bb
Muhammed <Olumide2956@gmail.com>
2023-05-30 23:43:02 -0700

    empty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,10 +10,16 @@
 
 void print_alphabet(void)
 {
-	char letter;
-
-	(for letter = 'a'; letter <= 'z'; letter++)
-	       _putchar(letter);
-
-	_putchar ('\n');
+	char 'c' = a;
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n')
+}
+int main(void)
+{
+	print_alphabet();
+	return (0);
 }
