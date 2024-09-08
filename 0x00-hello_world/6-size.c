661203fa085f3e8ff3cc62ad8097e9e7991c058c
Olumidelala <olumide2956@gmail.com>
2024-09-08 14:22:48 -0700

    multiple func that prints sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/* Print sizes for thirty-bit compilation */
+printf("Size of a char: %d byte(s)\n", (int)sizeof(char));
+printf("Size of an int: %d byte(s)\n", (int)sizeof(int));
+printf("Size of a long int: %d byte(s)\n", (int)sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", (int)sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", (int)sizeof(float));
+
+/* Print sizes for 32-bit compilation */
+printf("Size of a char: %d byte(s)\n", (int)sizeof(char));
+printf("Size of an int: %d byte(s)\n", (int)sizeof(int));
+printf("Size of a long int: %d byte(s)\n", (int)sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", (int)sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", (int)sizeof(float));
+
+return (0);
+}
+
