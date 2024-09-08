1db6896385674324b52b9d7e4b215837a590aa95
Olumidelala <olumide2956@gmail.com>
2024-09-08 14:56:35 -0700

    okay this size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -18,4 +18,6 @@ printf("Size of an int: %d byte(s)\n", (int)sizeof(int));
 printf("Size of a long int: %d byte(s)\n", (int)sizeof(long int));
 printf("Size of a long long int: %d byte(s)\n", (int)sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", (int)sizeof(float));
+return (0);
+}
 
