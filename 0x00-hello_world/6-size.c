3aaad526997481c3e904e2c044a5da25206900ac
Olumidelala <olumide2956@gmail.com>
2024-09-08 15:14:50 -0700

    fun size,pls work

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,12 @@
  */
 int main(void)
 {
-/* Check if compiling for 64-bit */
-printf("64-bit compilation:\n");
-printf("32-bit compilation:\n");
+/* Uncomment the following line to indicate 64-bit compilation */
+/* printf("64-bit compilation:\n"); */
+
+/* Uncomment the following line to indicate 32-bit compilation */
+/* printf("32-bit compilation:\n"); */
+
 printf("Size of a char: %d byte(s)\n", (int)sizeof(char));
 printf("Size of an int: %d byte(s)\n", (int)sizeof(int));
 printf("Size of a long int: %d byte(s)\n", (int)sizeof(long int));
@@ -17,4 +20,3 @@ printf("Size of a long long int: %d byte(s)\n", (int)sizeof(long long int));
 printf("Size of a float: %d byte(s)\n", (int)sizeof(float));
 return (0);
 }
-
