7eef0717c6a0a0495c531eaff1df683b7e5d8a10
Olumidelala <olumide2956@gmail.com>
2024-09-08 15:01:55 -0700

    now size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,12 +7,9 @@
  */
 int main(void)
 {
-	#ifdef __x86_64__ /* Check if compiling for 64-bit */
+/* Check if compiling for 64-bit */
 printf("64-bit compilation:\n");
-#else
 printf("32-bit compilation:\n");
-#endif
-
 printf("Size of a char: %d byte(s)\n", (int)sizeof(char));
 printf("Size of an int: %d byte(s)\n", (int)sizeof(int));
 printf("Size of a long int: %d byte(s)\n", (int)sizeof(long int));
