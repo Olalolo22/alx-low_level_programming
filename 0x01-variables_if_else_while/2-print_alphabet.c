5da95285eaa1497435698e975e7f0422f611c0c9
Olumidelala <olumide2956@gmail.com>
2024-09-17 18:53:47 -0700

    print alphabet till end

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+char ap = 'a';
+while (ap <= 'z')
+{
+putchar(ap);
+ap++;
+}
+putchar('\n');
+return (0);
+}
