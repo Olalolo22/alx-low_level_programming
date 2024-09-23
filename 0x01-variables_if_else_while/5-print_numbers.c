c3af004ea128606ed65ac8066936b09fdf56d84d
Olumidelala <olumide2956@gmail.com>
2024-09-23 00:09:29 -0700

    prints base10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always (0) Success
+ */
+
+int main(void)
+{
+	for (int dem = 0; dem < 10; dem++
+			{
+			printf("%d\n", dem);
+			}
+			return (0);
+}
